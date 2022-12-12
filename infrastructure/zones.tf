0f5f9d8a1e4bd0032696eec5ffcaf99dee3f44e3
Matt Crook <mattlcrook@icloud.com>
2022-12-12 23:00:34 +1300

    feat: add routing to fg

--- infrastructure/zones.tf
+++ infrastructure/zones.tf
@@ -1,10 +1,21 @@
+resource "cloudflare_record" "mc-fg" {
+  zone_id = local.zone_id
+  type    = "CNAME"
+
+  name  = "froggrove.mc"
+  value = local.home_hostname
+  ttl   = 1 # automatic
+
+  proxied = false
+}
+
 resource "cloudflare_record" "mc-sb3" {
   zone_id = local.zone_id
   type    = "CNAME"
 
-  name    = "sb3.mc"
-  value   = local.home_hostname
-  ttl     = 1 # automatic
+  name  = "sb3.mc"
+  value = local.home_hostname
+  ttl   = 1 # automatic
 
   proxied = false
 }
